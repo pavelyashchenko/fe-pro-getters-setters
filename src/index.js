d80f1ae65ce26b202693a3a80d4a992164d25a2e
Pavel Yashchenko <yashchenkops@gmail.com>
2022-12-14 00:28:11 +0200

    done 12 homework

--- src/index.js
+++ src/index.js
@@ -20,4 +20,34 @@ export const school = {
     6: new Student('Eugene', [97, 34, 78, 85, 98, 65]),
     7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
   },
+  
+  mainGetter(gradeType, minGrade, maxGrade) {
+
+    Object.defineProperty(this, gradeType, {
+      get() {
+        const studArr = Object.values(school.students);
+
+        let gradeFilter = studArr.filter(function (item) {
+          if (item.averageGrade <= maxGrade && item.averageGrade >= minGrade) {
+            return item;
+          }
+        });
+
+        let gradeNames = gradeFilter.map(function (item) {
+          return item.name;
+        });
+
+        return gradeNames.join(', ');
+      },
+    });
+  },
+
+  limitGrade() {
+    this.mainGetter('aGradeStudents', 90, 100);
+    this.mainGetter('bGradeStudents', 75, 89);
+    this.mainGetter('cGradeStudents', 60, 75);
+    this.mainGetter('dGradeStudents', 0, 59);
+  },
 };
+
+school.limitGrade();
